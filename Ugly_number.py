fb369ac0a084724ae2d6a9ef330970b4c1a0001c
KOMALJIT SINGH <31124879+komaljit@users.noreply.github.com>
2017-11-04 23:55:12 -0700

    Create Ugly_number.py

---
+++ Ugly_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    # @param {integer} num
+    # @return {boolean}
+    def isUgly(self, num):
+        if num == 0:
+            return False
+        for i in [2, 3, 5]:
+            while num % i == 0:
+                num /= i
+        return num == 1
