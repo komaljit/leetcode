fd54d6ee944ef15fd8f44e04612c333653740704
KOMALJIT SINGH <31124879+komaljit@users.noreply.github.com>
2018-02-04 09:37:26 -0800

    Create Queue with two stacks.py

---
+++ Queue with two stacks.py
@@ -0,0 +1,65 @@
+class MyQueue(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.l1 = []
+        self.l2 = [] 
+        
+        
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: void
+        """
+        self.l1.append(x)
+
+        
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        if not len(self.l2):
+            try:
+                while True:
+                    self.l2.append(self.l1.pop())
+            except:
+                pass
+        try:
+            return self.l2.pop()
+        except:
+            return None       
+        
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        top = self.pop()  
+        self.l2.append(top)
+        return top
+        
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        if len(self.l1) or len(self.l2):
+            return False
+        return True        
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
