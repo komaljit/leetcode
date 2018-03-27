7ab91158bd5bb2987006354f1e4223942d456733
KOMALJIT SINGH <31124879+komaljit@users.noreply.github.com>
2018-03-26 23:52:30 -0700

    Create Binary_tree_order_traversal.js

---
+++ Javascript/Binary_tree_order_traversal.js
@@ -0,0 +1,17 @@
+var traversal = function(ret, root, depth) {
+    if(root === null) {
+        return;
+    }
+    if(!Array.isArray(ret[depth])) {
+        ret[depth] = [];
+    }
+    ret[depth].push(root.val);
+    traversal(ret, root.left, depth + 1);
+    traversal(ret, root.right, depth + 1);
+}
+
+var levelOrder = function(root) {
+    var ret = [];
+    traversal(ret, root, 0);
+    return ret;
+};
